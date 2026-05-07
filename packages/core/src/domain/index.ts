496a15a0c9e23ce3cdc9e397663c2c4c7b203568
Michele Memoli <michele.memoli@gmail.com>
2026-05-07 22:58:02 +0100

    refactor(core): removed handler

--- packages/core/src/domain/index.ts
+++ packages/core/src/domain/index.ts
@@ -1,6 +1,5 @@
 export * as Client from "./client";
 export * as Events from "./events";
-export * as Handlers from "./handler";
 export * as Messages from "./messages";
 export * as Providers from "./provider";
 export * as User from "./user";
